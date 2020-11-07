8dd269f1704fb255dd8e90d1dc1dd1978d288325
Kishan Mandora <kishanmandora7@gmail.com>
2020-11-07 23:05:14 +0530

    fetched the first few images

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const gallery = document.querySelector(".gallery");
+
 const apiKey = "S0mkkheay0twKtgoj_RVMFToAsxaj0U86qKCMXhOZc4";
+const listUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&per_page=5&page=1`;
+
+const getImgs = async () => {
+  const resp = await fetch(listUrl);
+  const data = await resp.json();
+  console.log(data);
+
+  data.forEach((photo) => {
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+
+    gallery.appendChild(img);
+  });
+};
+
+getImgs();
