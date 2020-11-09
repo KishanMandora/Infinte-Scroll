881e5661066acce91697fc2dfd24452d772e8adb
Kishan Mandora <kishanmandora7@gmail.com>
2020-11-09 19:11:57 +0530

    adding the loader functionality

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const gallery = document.querySelector(".gallery");
-
+const loader = document.querySelector(".loader");
+// const loader = document.querySelector(".loader");
+let initialLoad = 0;
+let totalLoad = 0;
+let photoArr = [];
 const apiKey = "S0mkkheay0twKtgoj_RVMFToAsxaj0U86qKCMXhOZc4";
 const listUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&per_page=5&page=1`;
 
+function imgLoad() {
+  initialLoad++;
+  if (initialLoad === totalLoad) {
+    loader.hidden = true;
+  }
+}
+
 const getImgs = async () => {
   const resp = await fetch(listUrl);
-  const data = await resp.json();
-  console.log(data);
+  const photoArr = await resp.json();
 
-  data.forEach((photo) => {
-    const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-
-    const user = document.createElement("p");
-
-    gallery.appendChild(img);
-    user.innerText = `${photo.user.first_name} ${photo.user.last_name}`;
-    gallery.appendChild(user);
+  totalLoad = photoArr.length;
+  photoArr.forEach((photo) => {
+    const imgDiv = document.createElement("div");
+    imgDiv.innerHTML = `<img src=${photo.urls.regular} alt=${photo.alt_description} />
+    <p> ${photo.user.first_name} ${photo.user.last_name}</p>`;
+    imgLoad();
+    gallery.appendChild(imgDiv);
   });
 };
 
